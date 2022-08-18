abfe50c0db516ca56275c9efcdb87cee56294925
Letícia Oliveira Andrade <leticiaoliveiraandrade.9@gmail.com>
2022-08-17 21:16:12 -0300

    feat: criação das migrations de users e teams

---
+++ app/backend/src/database/migrations/20220817230853-create-teams.js
@@ -0,0 +1,22 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('teams',{
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+      },
+      teamName: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+        field:'team_name',
+      }
+    })
+  },
+
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('teams')
+  }
+};
