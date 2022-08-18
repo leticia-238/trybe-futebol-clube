dcd0bba3d985360a25379bb10c66e407cfab7ce2
Letícia Oliveira Andrade <leticiaoliveiraandrade.9@gmail.com>
2022-08-18 15:37:18 -0300

    feat: configura model User do sequelize

---
+++ app/backend/src/database/models/User.ts
@@ -0,0 +1,45 @@
+import { Model, DataTypes } from 'sequelize';
+import db from '.';
+
+type RoleType = 'admin' | 'user';
+
+class User extends Model {
+  declare id: number;
+  declare username: string;
+  declare role: RoleType;
+  declare email: string;
+  declare password: string;
+}
+
+User.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  role: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+}, {
+  sequelize: db,
+  modelName: 'User',
+  underscored: true,
+  timestamps: false,
+});
+
+export default User;
