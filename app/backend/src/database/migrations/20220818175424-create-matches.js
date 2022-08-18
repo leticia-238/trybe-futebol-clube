659e8b86963eda7fd20555b5287d930bd151262c
Letícia Oliveira Andrade <leticiaoliveiraandrade.9@gmail.com>
2022-08-18 15:34:25 -0300

    feat: adiciona migration da tabela matches

---
+++ app/backend/src/database/migrations/20220818175424-create-matches.js
@@ -0,0 +1,57 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('matches', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      homeTeam: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: {
+            tableName: 'teams',
+            schema: 'schema',
+          },
+          key: 'id',
+        },
+        field: 'home_team',
+      },
+      homeTeamGoals: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        field: 'home_team_goals',
+      },
+      awayTeam: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: {
+            tableName: 'teams',
+            schema: 'schema',
+          },
+          key: 'id',
+        },
+        field: 'away_team',
+      },
+      awayTeamGoals: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        field: 'away_team_goals',
+      },
+      inProgress: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        field: 'in_progress',
+      },
+    });
+  },
+
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('matches');
+  },
+};
