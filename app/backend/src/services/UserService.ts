edc036f5185929a3c8f8c8e6f7a9f1b96b6322e3
Letícia Oliveira Andrade <leticiaoliveiraandrade.9@gmail.com>
2022-08-23 17:09:14 -0300

    feat: incrementa validação do login

--- app/backend/src/services/UserService.ts
+++ app/backend/src/services/UserService.ts
@@ -2,6 +2,7 @@
 import { IUser } from '../interfaces/User';
 // import User from '../database/models/User';
 import ValidationError from '../errors/ValidationError';
+import UnauthorizedError from '../errors/UnauthorizedError';
 
 class UserService {
   // static async findOne(payload: IUser): Promise<IUser> {
@@ -16,6 +17,16 @@ class UserService {
   static async validateSigninPayload(payload: IUser): Promise<void> {
     const hasKeys = 'email' in payload && 'password' in payload;
     if (!hasKeys) throw new ValidationError('All fields must be filled');
+
+    const { email, password } = payload;
+    const isString = typeof email === 'string' && typeof password === 'string';
+    // regex copiado da seguinte fonte: https://regexr.com/3e48o
+    const validEmail = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email);
+    const validPassword = password.length > 6;
+
+    if (!isString || !validEmail || !validPassword) {
+      throw new UnauthorizedError('Incorrect email or password');
+    }
   }
 }
 
