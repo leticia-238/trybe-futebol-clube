e6f2ef4b91392df4fdc216ab52e1262b364b11af
Letícia Oliveira Andrade <leticiaoliveiraandrade.9@gmail.com>
2022-08-23 15:58:41 -0300

    fix: corrige erro do http response

--- app/backend/src/middlewares/errorHandler.ts
+++ app/backend/src/middlewares/errorHandler.ts
@@ -3,8 +3,9 @@ import { ErrorRequestHandler } from 'express';
 const errorHandler: ErrorRequestHandler = (err, _req, res, _next) => {
   if (err.code) {
     res.status(err.code).json({ message: err.message });
+  } else {
+    res.status(500).json({ message: err.message });
   }
-  res.status(500).json({ message: err.message });
 };
 
 export default errorHandler;
