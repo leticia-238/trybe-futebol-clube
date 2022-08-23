52010fbcd59c6ba305a16db62151cd4a8d0e1083
Letícia Oliveira Andrade <leticiaoliveiraandrade.9@gmail.com>
2022-08-23 17:07:34 -0300

    test: testes da rota de login

--- app/backend/src/tests/login.test.ts
+++ app/backend/src/tests/login.test.ts
@@ -17,21 +17,22 @@ type ResponseType = {
   body: Record<BodyKeys, string>;
 };
 
-const BAD_REQUEST = 400
+const BAD_REQUEST = 400;
+const UNAUTHORIZED = 401;
 
 const { expect } = chai;
 
 describe("Teste Login", () => {
-  let requester = chai.request(app).keepOpen()
+  let requester = chai.request(app).keepOpen();
 
   describe("Login válido", () => {
     let chaiHttpResponse: ResponseType;
     before(async () => {
       sinon.stub(User, "findOne").resolves(mockUserLogin as User);
-      sinon.stub(AuthService, 'generateToken').returns(mockUserToken);
+      sinon.stub(AuthService, "generateToken").returns(mockUserToken);
       chaiHttpResponse = await requester
         .post("/login")
-        .send({ email: "email@.com", password: "123456"});
+        .send({ email: "email@email.com", password: "123456abc" });
     });
 
     after(sinon.restore);
@@ -45,26 +46,56 @@ describe("Teste Login", () => {
   });
 
   describe("Login inválido", () => {
-    let chaiHttpResponses: ResponseType[]
-    
-    before(async () => {
-      sinon.stub(User, "findOne")
-      chaiHttpResponses = await Promise.all([
-        requester.post("/login").send({ password: "123456" }),
-        requester.post("/login").send({ email: "email@email.com" }),
-      ])
+    let chaiHttpResponses: ResponseType[];
+
+    before(() => {
+      sinon.stub(User, "findOne");
     });
 
     after(sinon.restore);
 
-    it("deve retornar um status 400", async () => {
-      expect(chaiHttpResponses[0].status).to.equal(BAD_REQUEST);
-      expect(chaiHttpResponses[1].status).to.equal(BAD_REQUEST);
+    describe("requisição com campos faltando", () => {
+      before(async () => {
+        chaiHttpResponses = await Promise.all([
+          requester.post("/login").send({ password: "123456" }),
+          requester.post("/login").send({ email: "email@email.com" }),
+        ]);
+      });
+
+      it("deve retornar um status 400", async () => {
+        expect(chaiHttpResponses[0].status).to.equal(BAD_REQUEST);
+        expect(chaiHttpResponses[1].status).to.equal(BAD_REQUEST);
+      });
+      it("deve retornar uma mensagem de erro", async () => {
+        const message = "All fields must be filled";
+        expect(chaiHttpResponses[0].body).to.deep.equal({ message });
+        expect(chaiHttpResponses[1].body).to.deep.equal({ message });
+      });
     });
-    it("deve retornar uma mensagem de erro", async () => {
-      const message = "All fields must be filled"
-      expect(chaiHttpResponses[0].body).to.deep.equal({ message });
-      expect(chaiHttpResponses[1].body).to.deep.equal({ message });
+
+    describe("requisição com campos inválidos", () => {
+      before(async () => {
+        chaiHttpResponses = await Promise.all([
+          requester.post("/login").send({
+            email: "invalid",
+            password: "123456abc",
+          }),
+          requester.post("/login").send({
+            email: "email@email.com",
+            password: "123",
+          }),
+        ]);
+      });
+
+      it("deve retornar um status 401", async () => {
+        expect(chaiHttpResponses[0].status).to.equal(UNAUTHORIZED);
+        expect(chaiHttpResponses[1].status).to.equal(UNAUTHORIZED);
+      });
+      it("deve retornar uma mensagem de erro", async () => {
+        const message = "Incorrect email or password";
+        expect(chaiHttpResponses[0].body).to.deep.equal({ message });
+        expect(chaiHttpResponses[1].body).to.deep.equal({ message });
+      });
     });
   });
 });
