8b3d4fa3eafa193c1b06d0d9fc4b0c2f55cba4b2
Letícia Oliveira Andrade <leticiaoliveiraandrade.9@gmail.com>
2022-08-23 16:08:22 -0300

    feat: adiciona validação dos campos de login

--- app/backend/src/controllers/UserController.ts
+++ app/backend/src/controllers/UserController.ts
@@ -1,11 +1,12 @@
 import { Request, Response } from 'express';
-// import UserService from '../services/UserService';
+import UserService from '../services/UserService';
 import AuthService from '../services/AuthService';
 
 class UserController {
   static async signin(req: Request, res: Response) {
     const payload = req.body;
-    // await UserService.validatePayload();
+    await UserService.validateSigninPayload(payload);
+    // await UserService.findOne(payload);
     const token = AuthService.generateToken(payload);
     res.status(200).json({ token });
   }
--- app/backend/src/services/UserService.ts
+++ app/backend/src/services/UserService.ts
@@ -1,6 +1,22 @@
+// import * as Joi from 'joi';
+import { IUser } from '../interfaces/User';
+// import User from '../database/models/User';
+import ValidationError from '../errors/ValidationError';
+
 class UserService {
-  // static async validatePayload() {
+  // static async findOne(payload: IUser): Promise<IUser> {
+  //   const user = await User.findOne({
+  //     where: { ...payload },
+  //     attributes: { exclude: ['password'] },
+  //     raw: true,
+  //   });
+  //   return user as IUser;
   // }
+
+  static async validateSigninPayload(payload: IUser): Promise<void> {
+    const hasKeys = 'email' in payload && 'password' in payload;
+    if (!hasKeys) throw new ValidationError('All fields must be filled');
+  }
 }
 
 export default UserService;
