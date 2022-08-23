a015f3fe9ffda65431b4c12f9f9869638fd14c5c
Letícia Oliveira Andrade <leticiaoliveiraandrade.9@gmail.com>
2022-08-23 16:12:59 -0300

    tests: testes do login

--- app/backend/src/tests/login.test.ts
+++ app/backend/src/tests/login.test.ts
@@ -6,16 +6,9 @@ import chaiHttp = require("chai-http");
 import User from "../database/models/User";
 import { app } from "../app";
 import AuthService from "../services/AuthService";
+import { mockUserLogin, mockUserToken } from "./mocks/user";
 
 chai.use(chaiHttp);
-
-const mockLogin = {
-  email: "string",
-  password: "string",
-};
-
-const mockToken = "token123456";
-const mockErrorMessage = "message";
 
 type BodyKeys = "token" | "message";
 
@@ -24,15 +17,21 @@ type ResponseType = {
   body: Record<BodyKeys, string>;
 };
 
+const BAD_REQUEST = 400
+
 const { expect } = chai;
 
 describe("Teste Login", () => {
-  let chaiHttpResponse: ResponseType;
+  let requester = chai.request(app).keepOpen()
 
   describe("Login válido", () => {
+    let chaiHttpResponse: ResponseType;
     before(async () => {
-      sinon.stub(User, "findOne").resolves(mockLogin as User);
-      chaiHttpResponse = await chai.request(app).post("/login");
+      sinon.stub(User, "findOne").resolves(mockUserLogin as User);
+      sinon.stub(AuthService, 'generateToken').returns(mockUserToken);
+      chaiHttpResponse = await requester
+        .post("/login")
+        .send({ email: "email@.com", password: "123456"});
     });
 
     after(sinon.restore);
@@ -40,27 +39,32 @@ describe("Teste Login", () => {
     it("deve retornar um status 200", async () => {
       expect(chaiHttpResponse.status).to.equal(200);
     });
-
     it("deve retornar um token", async () => {
-      sinon.stub(AuthService, 'generateToken').returns(mockToken);
-      expect(chaiHttpResponse.body).to.equal({ token: mockToken });
+      expect(chaiHttpResponse.body).to.deep.equal({ token: mockUserToken });
     });
   });
 
   describe("Login inválido", () => {
+    let chaiHttpResponses: ResponseType[]
+    
     before(async () => {
-      sinon.stub(User, "findOne").rejects();
-      chaiHttpResponse = await chai.request(app).post("/login");
+      sinon.stub(User, "findOne")
+      chaiHttpResponses = await Promise.all([
+        requester.post("/login").send({ password: "123456" }),
+        requester.post("/login").send({ email: "email@email.com" }),
+      ])
     });
 
     after(sinon.restore);
 
     it("deve retornar um status 400", async () => {
-      expect(chaiHttpResponse.status).to.equal(400);
+      expect(chaiHttpResponses[0].status).to.equal(BAD_REQUEST);
+      expect(chaiHttpResponses[1].status).to.equal(BAD_REQUEST);
     });
-
     it("deve retornar uma mensagem de erro", async () => {
-      expect(chaiHttpResponse.status).to.equal({ message: mockErrorMessage });
+      const message = "All fields must be filled"
+      expect(chaiHttpResponses[0].body).to.deep.equal({ message });
+      expect(chaiHttpResponses[1].body).to.deep.equal({ message });
     });
   });
 });
---
+++ app/backend/src/tests/mocks/user.ts
@@ -0,0 +1,6 @@
+ export const mockUserLogin = {
+  email: "email@email.com",
+  password: "123456",
+};
+
+export const mockUserToken = "token123456";
