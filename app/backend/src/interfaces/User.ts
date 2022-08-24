badc87b5075ff9818f040b17c8ac8034c3e79b81
Letícia Oliveira Andrade <leticiaoliveiraandrade.9@gmail.com>
2022-08-24 19:51:24 -0300

    feat(interfaces/User): adiciona interface extendida

--- app/backend/src/interfaces/User.ts
+++ app/backend/src/interfaces/User.ts
@@ -1,8 +1,12 @@
 export type RoleType = 'admin' | 'user';
 
-export interface IUser {
-  username: string,
-  role: RoleType,
+export interface IUserLogin {
   email: string,
   password: string
+}
+
+export interface IUser extends IUserLogin {
+  id: number,
+  username: string,
+  role: RoleType,
 }
