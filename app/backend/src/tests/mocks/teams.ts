56a893cfd76ba050f36af7889997a29e0f5413a6
Letícia Oliveira Andrade <leticiaoliveiraandrade.9@gmail.com>
2022-08-26 12:24:23 -0300

    style: formatação de código

--- app/backend/src/tests/mocks/teams.ts
+++ app/backend/src/tests/mocks/teams.ts
@@ -1,12 +1,12 @@
 export const mockTeams = [
-  { id: 1, teamName: "Avaí/Kindermann" },
-  { id: 2, teamName: "Bahia" },
-  { id: 3, teamName: "Botafogo" },
-]
+  { id: 1, teamName: 'Avaí/Kindermann' },
+  { id: 2, teamName: 'Bahia' },
+  { id: 3, teamName: 'Botafogo' },
+];
 
-const id = 5
+const id = 5;
 
 export const mockTeam = {
   id,
-  teamName: "Cruzeiro"
-}
+  teamName: 'Cruzeiro',
+};
--- app/backend/src/tests/mocks/user.ts
+++ app/backend/src/tests/mocks/user.ts
@@ -1,16 +1,16 @@
-import { encryptPassword } from "../../utils/encrypt";
-import { IUserDB } from "../../interfaces/User";
+import { encryptPassword } from '../../utils/encrypt';
+import { IUserDB } from '../../interfaces/User';
 
-export const validEmail = "email@email.com";
-export const validPassword = "123456abc";
+export const validEmail = 'email@email.com';
+export const validPassword = '123456abc';
 export const hashPassword = encryptPassword(validPassword);
 
 export const mockUserLogin: IUserDB = {
   id: 1,
-  username: "user",
-  role: "admin",
+  username: 'user',
+  role: 'admin',
   email: validEmail,
   password: hashPassword,
 };
 
-export const mockUserToken = "token123456";
+export const mockUserToken = 'token123456';
