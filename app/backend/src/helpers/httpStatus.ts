c027e4cac343b431964329e6debf5b4adcd8ac0d
Letícia Oliveira Andrade <leticiaoliveiraandrade.9@gmail.com>
2022-08-28 17:52:12 -0300

    refactor(helpers/httpStatus): troca objeto por enum

--- app/backend/src/helpers/httpStatus.ts
+++ app/backend/src/helpers/httpStatus.ts
@@ -1,6 +1,9 @@
-export default {
-  ok: 200,
-  badRequest: 400,
-  unauthorized: 401,
-  notFound: 404,
-};
+enum Status {
+  ok = 200,
+  created = 201,
+  badRequest = 400,
+  unauthorized = 401,
+  notFound = 404,
+}
+
+export default Status;
